8ed7b796524424e28a8d59a80480d9bc5f4efda1
noerinor <shymoniavl@gmail.com>
2025-01-29 15:46:46 +0200

    LINQ2

---
+++ LINQ2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Actor
+{
+    public string Name { get; set; }
+    public DateTime Birthdate { get; set; }
+}
+
+abstract class ArtObject
+{
+    public string Author { get; set; }
+    public string Name { get; set; }
+    public int Year { get; set; }
+}
+
+class Film : ArtObject
+{
+    public int Length { get; set; }
+    public IEnumerable<Actor> Actors { get; set; }
+}
+
+class Book : ArtObject
+{
+    public int Pages { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var data = new List<object>() {
+            "Hello",
+            new Book() { Author = "Terry Pratchett", Name = "Guards! Guards!", Pages = 810 },
+            new List<int>() {4, 6, 8, 2},
+            new string[] {"Hello inside array"},
+            new Film() { Author = "Martin Scorsese", Name= "The Departed", Actors = new List<Actor>() {
+                new Actor() { Name = "Jack Nickolson", Birthdate = new DateTime(1937, 4, 22)},
+                new Actor() { Name = "Leonardo DiCaprio", Birthdate = new DateTime(1974, 11, 11)},
+                new Actor() { Name = "Matt Damon", Birthdate = new DateTime(1970, 8, 10)}
+            }},
+            new Film() { Author = "Gus Van Sant", Name = "Good Will Hunting", Actors = new List<Actor>() {
+                new Actor() { Name = "Matt Damon", Birthdate = new DateTime(1970, 8, 10)},
+                new Actor() { Name = "Robin Williams", Birthdate = new DateTime(1951, 8, 11)},
+            }},
+            new Book() { Author = "Stephen King", Name="Finders Keepers", Pages = 200},
+            "Leonardo DiCaprio"
+        };
+
+        // 1.
+        Console.WriteLine("1.___________");
+        Console.WriteLine(string.Join(", ", data.Where(x => !(x is ArtObject))));
+
+        // 2. 
+        Console.WriteLine("2.___________");
+        Console.WriteLine(string.Join(", ", data.OfType<Film>().SelectMany(f => f.Actors).Select(a => a.Name).Distinct()));
+
+        // 3. 
+        Console.WriteLine("3.___________");
+        Console.WriteLine(data.OfType<Film>().SelectMany(f => f.Actors).Count(a => a.Birthdate.Month == 8));
+
+        // 4. 
+        Console.WriteLine("4.___________");
+        Console.WriteLine(string.Join(", ", data.OfType<Film>().SelectMany(f => f.Actors).OrderBy(a => a.Birthdate).Take(2).Select(a => a.Name)));
+
+        // 5.
+        Console.WriteLine("5.___________");
+        Console.WriteLine(string.Join(", ", data.OfType<Book>().GroupBy(b => b.Author).Select(g => $"{g.Key}: {g.Count()}")));
+
+        // 6.
+        Console.WriteLine("6.___________");
+        Console.WriteLine(string.Join(", ", data.OfType<ArtObject>().GroupBy(a => a.Author).Select(g => $"{g.Key}: {g.Count()}")));
+
+        // 7. 
+        Console.WriteLine("7.___________");
+        Console.WriteLine(data.OfType<Film>().SelectMany(f => f.Actors).SelectMany(a => a.Name).Distinct().Count());
+
+        // 8.
+        Console.WriteLine("8.___________");
+        Console.WriteLine(string.Join(", ", data.OfType<Book>().OrderBy(b => b.Author).ThenBy(b => b.Pages).Select(b => b.Name)));
+
+        // 9. 
+        Console.WriteLine("9.___________");
+        Console.WriteLine(string.Join("\n", data.OfType<Film>().SelectMany(f => f.Actors, (f, a) => new { a.Name, Film = f.Name })
+            .GroupBy(x => x.Name)
+            .Select(g => $"{g.Key}: {string.Join(", ", g.Select(f => f.Film))}")));
+
+        // 10. 
+        Console.WriteLine("10.___________");
+        Console.WriteLine(data.OfType<Book>().Sum(b => b.Pages) + data.OfType<IEnumerable<int>>().SelectMany(i => i).Sum());
+
+        // 11. 
+        Console.WriteLine("11.___________");
+        var bookDictionary = data.OfType<Book>().GroupBy(b => b.Author).ToDictionary(g => g.Key, g => g.Select(b => b.Name).ToList());
+        Console.WriteLine(string.Join("\n", bookDictionary.Select(kvp => $"{kvp.Key}: {string.Join(", ", kvp.Value)}")));
+
+        // 12.
+        Console.WriteLine("12.___________");
+        Console.WriteLine(string.Join(", ", data.OfType<Film>()
+            .Where(f => f.Actors.Any(a => a.Name == "Matt Damon"))
+            .Where(f => !f.Actors.Any(a => data.OfType<string>().Contains(a.Name)))
+            .Select(f => f.Name)));
+    }
+}
